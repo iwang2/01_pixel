1927e4935f664280a147823821c5cecc8000c4bb
iwang2 <iwang2@stuy.edu>
2018-02-02 15:19:09 -0500

    wooo!

--- pixel.c
+++ pixel.c
@@ -5,26 +5,22 @@
 #include <string.h>
 
 int main(){
-  int size = 10;
+  int width = 256;
+  int height = 256;
   char buff[sizeof("100 100 200 ")];
-  char s[sizeof("100 100 200 ") * size * size];
-  sprintf(s, "P3 %d %d %d ", size, size, 255);
+  char s[sizeof("100 100 200 ") * width * height];
+  sprintf(s, "P3 %d %d %d  ", width, height, 255);
 
   int i, j;
-  for(i = 0; i < 10; i++){
-    for(j = 0; j < 10; j++){
+  for(i = 0; i < width; i++){
+    for(j = 0; j < height; j++){
       sprintf(buff, "%d %d %d  ", i, j, i+j);
       strcat(s, buff);
       memset(buff, 0, sizeof(buff));
     }
   }
-  /*
-  size = strlen(s) / sizeof(char);
-  s[size-1] = 0;
-  */
   
   int fd = open("image.ppm", O_CREAT | O_WRONLY, 0644);
   write(fd, s, strlen(s));
-  
   close(fd);
 }
