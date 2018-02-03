4be4a2959c2e680f3c1346ab13b5a56a29652549
iwang2 <iwang2@stuy.edu>
2018-02-03 00:21:20 -0500

    slightly different image and faster speed

--- pixel.c
+++ pixel.c
@@ -5,26 +5,50 @@
 #include <string.h>
 
 int main(){
-  int width = 256;
-  int height = 256;
-  char buff[sizeof("100 100 200 ")];
-  char s[sizeof("100 100 200 ") * width * height * 4];
-  sprintf(s, "P3\n%d\n%d\n%d\n", width * 2, height * 2, 255);
-
-  int i, j, k;
-  for(i = 0; i < width; i++){
-    for(k = 0; k < 2; k++){
-      for(j = 0; j < height; j++){
-	sprintf(buff, "%d %d %d  ", i, j, i+j);
-	strcat(s, buff);
-	strcat(s, buff);
-	//memset(buff, 0, sizeof(buff));
-      }
-      strcat(s, "\n");
-    }
-  }
   
   int fd = open("image.ppm", O_CREAT | O_WRONLY, 0644);
-  write(fd, s, strlen(s));
+  
+  int width = 256 * 2;
+  int height = 256 * 2;
+  
+  char buff[sizeof("255 255 255  ")];
+  sprintf(buff, "P3\n%d\n%d\n%d\n", width, height, 255);
+  write(fd, buff, strlen(buff));
+
+  int i, j, k;
+  for(i = 0; i < width/2; i++){
+    for(j = 0; j < height/2; j++){
+      k = i + j;
+      if (k > 255) k -= 256;
+      sprintf(buff, "%d %d %d  ", i, j, k);
+      write(fd, buff, strlen(buff));
+    }
+    for(j = height/2-1; j >= 0; j--){
+      k = i + j;
+      if (k > 255) k -= 256;
+      sprintf(buff, "%d %d %d  ", i, j, k);
+      write(fd, buff, strlen(buff));
+    }
+    write(fd, "\n", strlen("\n"));
+  }
+  
+  for(i = width/2-1; i >= 0; i--){
+    for(j = 0; j < height/2; j++){
+      k = i + j;
+      if (k > 255) k -= 256;
+      sprintf(buff, "%d %d %d  ", i, j, k);
+      write(fd, buff, strlen(buff));
+    }
+    for(j = height/2-1; j >= 0; j--){
+      k = i + j;
+      if (k > 255) k -= 256;
+      sprintf(buff, "%d %d %d  ", i, j, k);
+      write(fd, buff, strlen(buff));
+    }
+    write(fd, "\n", strlen("\n"));
+  }
+  
   close(fd);
+  
+  return 0;
 }
